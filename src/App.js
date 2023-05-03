7cc0079a0aafafb8081374e2ccd9577f5a0bc6cb
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-05-03 11:42:26 +0200

    add set array, to fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import { useState } from "react";
 
 const App = () => {
   const [array, setArray] = useState([1,2,3,4,5])
-  const [num, setNum] = useState(0)
+  const [num, setNum] = useState('')
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log(num)
+    setArray(array.map(function(elem) {
+      if(elem === Number(num)) {
+        array[array.indexOf(elem)] = 100
+      }
+      else{
+        return elem
+      }
+    }))
+    console.log(array)
   }
 
   function handleChange(e) {
@@ -16,7 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input value={num} onChange={handleChange}/>
+        <input type='number' value={num} onChange={handleChange}/>
         <button type="submit">Submit form</button>
       </form>
     </div>
