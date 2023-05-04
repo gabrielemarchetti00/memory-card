9845ef6101c263bb433ba3953216219f8f93b6d6
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-05-04 17:02:52 +0200

    add images and fix click

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ const App = () => {
   
   return (
     <div className="App">
+      <div className="header">
+        <div className="title">Disney Memory</div>
+        <div className="score">
+          <div className="current-score">Current Score: </div>
+          <div className="best-score">Best Score: </div>
+        </div>
+      </div>
       <CardDisplay/>
     </div>
   );
--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -1,22 +1,77 @@
 import React from "react";
 import uniqid from "uniqid";
 import { useState} from "react";
+import eldorado from '../images/eldorado.jpeg'
+import gobbo from '../images/gobbo.jpg'
+import hercules from '../images/hercules.webp'
+import mulan from '../images/mulan.jpg'
+import olaf from '../images/olaf.jpeg'
+import peterpan from '../images/peterpan.jpg'
+import robin from '../images/robin.jpg'
+import simba from '../images/simba.webp'
+import spirit from '../images/spirit.jpg'
+import tarzan from '../images/tarzan.jpg'
 
 const cardArray = [
     {
       text: 1,
       id: uniqid(),
-      pick: false
+      pick: false,
+      image: eldorado
     },
     {
       text: 2,
       id: uniqid(),
-      pick: false
+      pick: false,
+      image: gobbo
     },
     {
       text: 3,
       id: uniqid(),
-      pick: false
+      pick: false,
+      image: hercules
+    },
+    {
+      text: 4,
+      id: uniqid(),
+      pick: false,
+      image: mulan
+    },
+    {
+      text: 5,
+      id: uniqid(),
+      pick: false,
+      image: olaf
+    },
+    {
+      text: 6,
+      id: uniqid(),
+      pick: false,
+      image: peterpan
+    },
+    {
+      text: 7,
+      id: uniqid(),
+      pick: false,
+      image: robin
+    },
+    {
+      text: 8,
+      id: uniqid(),
+      pick: false,
+      image: simba
+    },
+    {
+      text: 9,
+      id: uniqid(),
+      pick: false,
+      image: spirit
+    },
+    {
+      text: 10,
+      id: uniqid(),
+      pick: false,
+      image: tarzan
     }
   ]
 
@@ -34,10 +89,12 @@ const CardDisplay = () => {
     }
 
     return (
-        <div className="cards">
+        <div className="card-container">
             {cards.map((card) => {
-            return <div key={card.id} onClick={handleClick} id={card.text}>{card.text}</div>;
-            })}
+            return <div className="card" key={card.id}>
+                    <img src={card.image} alt='card-img' onClick={handleClick} id={card.text}></img>
+                   </div>;
+             })}
         </div>
     );
 }
